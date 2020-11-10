331aa42d112460f5f54799f359c9a9b0260cb377
rachellee114 <71678213+rachellee114@users.noreply.github.com>
2020-11-09 17:28:05 -0800

    Add files via upload

---
+++ snakegame.py
@@ -0,0 +1,60 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        pass
+        
+    def move(self, dirnx, dirny):
+        pass
+    
+    def draw(self, surface, eyes=False):
+        pass
+        
+
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+        
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+        
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    pass
+        
+
+def redrawWindow(surface):
+    pass
+
+
+def randomSnack(rows, item):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    win = pygame.display.set_mode()
+
+
+
+main()
